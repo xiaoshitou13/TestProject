304263efce65dbf3941f2a6199ee1b5ae4ecd761
baiyuchun <baiyuchun@yuyisheng.com.cn>
2020-05-23 10:19:02 +0800

    v1.0.1 稳定版

---
+++ app/src/main/java/com/bewg/testproject/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bewg.testproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //1.0.1版本项目
+    }
+}
